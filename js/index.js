7c882896b2708e2c63a5eb162af3310dcefe4303
Esteban Arias <earias@plan.ceibal.edu.uy>
2016-02-15 20:04:04 -0300

    imagenes de teclado
    imagenes de teclado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 
-angular.module('MyApp')
+angular.module('MyApp',  ['ngMaterial'])
 
 .controller('AppCtrl', function($scope) {
-
+//      $scope.categories = [
+//        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+//        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+//        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+//      ];
+      
 });
