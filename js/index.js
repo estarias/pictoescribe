e0ab767e622885d1d2cbae1c10e4253f11f9371f
Esteban Arias <earias@plan.ceibal.edu.uy>
2016-02-15 20:26:02 -0300

    Agrega items del teclado iterando

--- js/index.js
+++ js/index.js
@@ -2,10 +2,32 @@
 angular.module('MyApp',  ['ngMaterial'])
 
 .controller('AppCtrl', function($scope) {
-//      $scope.categories = [
-//        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-//        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-//        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-//      ];
+      $scope.categories = [
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},        
+        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"}
+      ];
       
 });
