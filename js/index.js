7f93ec3074c1aa3d718c6642caf7683a004bfbb0
Esteban Arias <earias@plan.ceibal.edu.uy>
2016-02-16 22:07:18 -0300

    muestra primeros pictogramas de categorias y las escribe

--- js/index.js
+++ js/index.js
@@ -1,33 +1,28 @@
 
 angular.module('MyApp',  ['ngMaterial'])
 
-.controller('AppCtrl', function($scope) {
+.controller('AppCtrl', function($scope, $sce) {
       $scope.categories = [
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-         {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"},        
-        {id:48,name:"test1", icon:"img/icons/favorite-user.svg"}
+        {id:1,name:"animales", icon:"img/categorias/animales.png"},
+        {id:2,name:"comida", icon:"img/categorias/comida.png"},
+         {id:3,name:"frutas", icon:"img/categorias/frutas.png"},
+        {id:4,name:"numeros", icon:"img/categorias/numeros.png"},
+         {id:5,name:"objetos", icon:"img/categorias/objetos.png"},
+        {id:6,name:"verbos", icon:"img/categorias/verbos.png"}
+     
       ];
+      
+      $scope.text ="";
+      
+      //Select category
+      $scope.toggleCategory = function(it) {
+         //alert(it.name);
+         someHtmlVar = "<md-tooltip>" + it.name + "</md-tooltip>";
+         someHtmlVar += "<img src='"+ it.icon + "' class='keyboardItem' alt='"+it.name+"'>";
+         $scope.text += someHtmlVar;
+         $scope.thisCanBeusedInsideNgBindHtml = $sce.trustAsHtml($scope.text);
+    };
+    
+    
       
 });
