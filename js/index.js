b485fafe6dec51a4304c94b6ac3622800061aceb
Esteban Arias <earias@plan.ceibal.edu.uy>
2016-03-07 20:39:32 -0300

    agrega categorias recorriendo directorio

--- js/index.js
+++ js/index.js
@@ -2,19 +2,14 @@
 angular.module('MyApp',  ['ngMaterial'])
 
 .controller('AppCtrl', function($scope, $sce) {
-      $scope.categories = [
-        {id:1,name:"animales", icon:"img/categorias/animales.png", sound:"img/categorias/animales.mp3"},
-        {id:2,name:"comidas", icon:"img/categorias/comidas.png", sound:"img/categorias/comidas.mp3"},
-         {id:3,name:"frutas", icon:"img/categorias/frutas.png", sound:"img/categorias/frutas.mp3"},
-        {id:4,name:"numeros", icon:"img/categorias/numeros.png", sound:"img/categorias/numeros.mp3"},
-         {id:5,name:"objetos", icon:"img/categorias/objetos.png", sound:"img/categorias/objetos.mp3"},
-        {id:6,name:"verbos", icon:"img/categorias/verbos.png", sound:"img/categorias/verbos.mp3"}
-   
-      ];
-           
+      $scope.cats_name = [ "colores", "numeros" ];
+      $scope.categories = [];
+
+      make_categoies();
+      
       $scope.text = [];
       $scope.playbox = document.getElementById('playbox');
-      
+           
     //write text
       $scope.toggleCategory = function(it) {
           $scope.text.push(it);
@@ -60,5 +55,20 @@ angular.module('MyApp',  ['ngMaterial'])
                 if(index < $scope.sounds.length) playNext( index);          
         });
      } 
-      
+
+     function make_categoies(){
+        for (var i = 0; i < $scope.cats_name.length; i++) {
+            cat = $scope.cats_name[i];
+
+            var category = {
+                "name": cat,
+                "icon": "categorias/" + cat + "/" + cat + ".png",
+                "sound": "categorias/" + cat + "/" + cat + ".mp3",
+              };
+            $scope.categories.push(category);
+        }
+        
+     }
+     
 });
+
