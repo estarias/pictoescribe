95ea0d9966c2af05d78d62df16712bbedb7f24c6
Esteban Arias <earias@plan.ceibal.edu.uy>
2016-03-11 00:57:17 -0300

    agrega colores

--- js/index.js
+++ js/index.js
@@ -3,8 +3,11 @@ angular.module('MyApp',  ['ngMaterial'])
 
 .controller('AppCtrl', function($scope, $sce) {
     $scope.cats_name = [ 
-      {name:"colores", elementos:["amarillo", "azul"]},
-      {name:"numeros", elementos:["cero", "uno"]}    
+      {name:"abecedario", color:"red", elementos:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]},
+      {name:"colores", color:"yellow", elementos:["amarillo", "azul", "blanco", "dorado", "gris", "marron", "naranja", "negro", "rojo", "rosa", "verde","verde_claro", "violeta"]}  ,
+      {name:"numeros",color:"blue",  elementos:["cero", "numero (1)", "numero (2)", "numero (3)", "numero (4)", "numero (5)", "numero (6)", "numero (7)", "numero (8)", "numero (9)"]},    
+      {name:"personas",color:"green",  elementos:["yo", "yo_1", "yo_2", "yo_3", "tu", "ellos", "ellos_1", "ellos_2", "ellas", "ellas_1", "nosotros", "nosotras"]},
+      {name:"sociales",color:"red",  elementos:["¡es_gracioso!", "¡estoy_bien!", "¡felicidades!", "¡genial!", "¡lo_siento!", "¿cual_es_tu_email-", "¿cual_es_tu_nombre-", "¿cuantos_dias-", "¿puedo_ayudarte-", "¿que_dices-", "¿que_tal-"]}
     ];
 
     $scope.keys = [];
@@ -16,7 +19,7 @@ angular.module('MyApp',  ['ngMaterial'])
 
     $scope.toggleKey = function(it) {
        if (it.type == "category"){
-           show_elements(it.name);
+           show_elements(it);
        }else {
            write_key(it);
            show_categories();
@@ -57,8 +60,8 @@ angular.module('MyApp',  ['ngMaterial'])
         $scope.playbox.src = $scope.sounds[index];
         $scope.playbox.play(); 
         $scope.playbox.addEventListener('ended', function(){ 
-                index++;
-                if(index < $scope.sounds.length) playNext( index);          
+            index++;
+            if(index < $scope.sounds.length) playNext( index);          
         });
      } 
      
@@ -71,24 +74,26 @@ angular.module('MyApp',  ['ngMaterial'])
                 "name": cat.name,
                 "icon": "categorias/" + cat.name + "/" + cat.name + ".png",
                 "sound": "categorias/" + cat.name + "/" + cat.name + ".mp3",
+                "color": cat.color,
                 "type": "category"
             };
             add_key(category);
        }
     };
     
-    function show_elements(name){
+    function show_elements(pcat){
         clean_keys();
         for (var i = 0; i < $scope.cats_name.length; i++) {
             cat = angular.fromJson($scope.cats_name[i]);
-            if (cat.name == name){               
+            if (cat.name == pcat.name){               
                 for (var x = 0; x < $scope.cats_name[i].elementos.length; x++) {
                     e = angular.fromJson($scope.cats_name[i]);
                     element_name = e.elementos[x];
                     var element = {
                         "name": element_name,
-                        "icon": "categorias/" + name + "/elementos/" + element_name + ".png",
-                        "sound": "categorias/" + name + "/elementos/" + element_name + ".mp3",
+                        "icon": "categorias/" + pcat.name + "/elementos/" + element_name + ".png",
+                        "sound": "categorias/" + pcat.name + "/elementos/" + element_name + ".mp3",
+                        "color": pcat.color,
                         "type": "element"
                     };
                     add_key(element);
