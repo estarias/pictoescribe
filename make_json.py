750ddba1b4901b1ea00dfae30990e504dd7e7762
Esteban Arias <earias@ceibal.edu.uy>
2016-03-23 11:45:50 -0300

    genera json a partir del directorio

--- make_json.py
+++ make_json.py
@@ -1,17 +1,31 @@
 import os
 import json
+from os import listdir
+from os.path import isfile, join, basename, splitext
+
+
+def list_elements(path):
+    out = []
+    for f in listdir(path): 
+        base = basename(join(path,f))
+        name = splitext(base)[0]
+        if name not in out:
+            out.append(name)
+    out.sort()
+    return out
 
 def path_to_dict(path):
-    d = {'name': os.path.basename(path)}
-    if os.path.isdir(path):
-        d['type'] = "directory"
-        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
-(path)]
-    else:
-        d['type'] = "file"
+    d = {}
+    d['name'] = basename(path)
+    d['color'] = 'red'
+    d['elementos'] = list_elements(join(path,'elementos'))
     return d
 
-str_output = json.dumps(path_to_dict('categorias'))
+o=[]
+for f in listdir('categorias'):
+    o.append(path_to_dict(join('categorias',f)))
+
+str_output = json.dumps(o)
 
 f = open('output.json', 'w')
 f.write(str_output)
