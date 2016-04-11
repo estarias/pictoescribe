89707b8e85a90a199e3e20453dc4471ef34dd1ee
Esteban Arias <estarias@gmail.com>
2016-04-11 18:55:03 -0300

    agrega colores

--- make_json.py
+++ make_json.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 import os
 import json
 from os import listdir
@@ -14,10 +17,25 @@ def list_elements(path):
     out.sort()
     return out
 
+def exist_file(path):
+    return os.path.exists(path)
+
+def read_file(path):
+    f = open(path, 'rw')
+    text = f.read()
+    f.close()
+    return text
+
+def get_colour(path):
+    _colour_default = 'red'
+    if exist_file(path):
+        return read_file(path)
+    return _colour_default
+
 def path_to_dict(path):
     d = {}
     d['name'] = basename(path)
-    d['color'] = 'red'
+    d['color'] = get_colour(join(path, "color"))
     d['elementos'] = list_elements(join(path,'elementos'))
     return d
 
