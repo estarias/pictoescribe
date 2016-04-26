4e64c65e5e29ca4a9dbaf69bd5b9129a07a6128f
Esteban Arias <earias@plan.ceibal.edu.uy>
2016-04-25 21:39:39 -0300

    Agrega boton pantalla completa

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
-angular.module('MyApp',  ['ngMaterial', 'ngDraggable'])
+angular.module('MyApp',  ['ngMaterial', 'ngDraggable', 'FBAngular'])
 
-.controller('AppCtrl', function($scope, $sce, $window, $timeout) {
+.controller('AppCtrl', function($scope, $sce, $window, $timeout,Fullscreen) {
     $scope.cats_name = [{color: "blue", name: "colores", elementos: ["amarillo", "azul", "blanco", "dorado", "gris", "marron", "naranja", "negro", "rojo", "rosa", "verde", "verde_claro", "violeta"]}, {color: "orange", name: "animales", elementos: ["abeja", "ardilla", "ballenas", "caballos", "cabras", "camellos", "canario", "caracoles", "cebras", "cerdo", "cerdos", "chimpances", "cocodrilos", "conejos", "delfin", "gallina", "gallo", "gato", "iguanas", "leon", "leon_marino", "mariposa", "mono", "oso", "oso_panda", "pajaro", "pajaro (1)", "paloma", "pantera", "pato", "perro", "perro (1)", "pinguino", "polilla", "pulpo", "ranas", "rinoceronte", "ternera", "toro", "tortugas", "vaca", "ñandus"]}, {color: "blue", name: "adjetivos", elementos: ["aburridos", "acompañado", "adulta", "alto", "anterior", "anterior (1)", "aquel", "ausente", "autonomo", "bajo", "baratos", "blandos", "calientes", "central", "claros", "confusos", "cualquier", "curiosos", "dependiente", "derecha", "diferente", "dulce", "duro", "egoista", "enamorado", "enfermos", "entero", "equilibrado", "este", "estos", "extraño", "falso", "feliz", "feliz (1)", "feliz (2)", "feo", "flacos", "frio", "fuerte", "gordo", "grande", "guapo", "hermosa", "iguales", "irritado", "izquierda", "joven", "lento", "mas", "menos", "menos (1)", "mojado", "negativo", "nervioso", "ninguna", "ningunas", "ninguno", "nuevo", "oscuros", "otra", "pegajosos", "pequeña", "pesadas", "picante", "posterior", "primer", "rizados", "rugoso", "sana", "seco", "sentado", "sola", "solo", "suaves", "sucios", "toda", "toda (1)", "ultimo", "vacio", "verdadera", "viejo"]}, {color: "orange", name: "lugares", elementos: ["ambulatorio", "avenida", "bibliotecas", "cafeterias", "carceles", "casas", "cementerios", "cines", "circos", "colegios", "comisarias", "jugueteria", "parada_de_taxis", "plaza", "tienda_de_musica", "tiendas", "verduleria"]}, {color: "white", name: "abecedario", elementos: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]}, {color: "white", name: "numeros", elementos: ["cero", "numero (1)", "numero (2)", "numero (3)", "numero (4)", "numero (5)", "numero (6)", "numero (7)", "numero (8)", "numero (9)"]}, {color: "green", name: "verbos", elementos: ["abrir", "acompañar", "agrupar", "alcanzar", "almorzar", "anteceder", "apagar", "avanzar", "bajar", "borrar", "cancelar", "cantar", "cepillar_los_dientes", "cerrar", "comer", "comer (1)", "comunicarse", "construir", "continuar", "correr", "cortar", "cortar_el_pelo", "cumplir", "dar", "dormir", "duchar", "enfermar", "enseñar", "entrar", "enviar", "escribir", "escuchar", "hacer_fotos", "intentar", "jugar", "lavar", "leer", "llamar", "llegar", "llorar", "nacer", "pasear", "pegar", "peinar", "perfumar", "pintar", "pintar (1)", "pulsar", "quejarse", "reir (1)", "rodear", "salir", "separar", "silenciar", "subir", "tomar", "trabajar", "ver", "visitar"]}, {color: "pink", name: "sociales", elementos: ["cual_es_tu_email-", "cual_es_tu_nombre-", "cuantos_dias-", "cuidado", "es_gracioso!", "estoy_bien!", "felicidades!", "genial!", "lo_siento!", "puedo_ayudarte-", "que_dices-", "que_tal-"]}, {color: "yellow", name: "personas", elementos: ["ella", "ellas", "ellas_1", "ellos", "ellos_1", "ellos_2", "nosotras", "nosotros", "tu", "yo", "yo_1", "yo_2", "yo_3"]}, {color: "pink", name: "interrogativas", elementos: ["como", "cuantos", "cuantos (1)", "que", "quien-", "quienes"]}];
 
     $scope.keys = [];   
@@ -22,6 +22,15 @@ angular.module('MyApp',  ['ngMaterial', 'ngDraggable'])
         stop();
     };
 
+     $scope.goFullscreen = function () {
+        if (Fullscreen.isEnabled())
+           Fullscreen.cancel();
+        else
+           Fullscreen.all();
+        // Set Fullscreen to a specific element (bad practice)
+        // Fullscreen.enable( document.getElementById('img') )
+    };
+    
     $scope.toggleKey = function(it) {
        if (it.type == "category"){
            show_elements(it);
@@ -198,6 +207,8 @@ angular.module('MyApp',  ['ngMaterial', 'ngDraggable'])
 
         return true;
     } 
+    
+
 });
 
 
