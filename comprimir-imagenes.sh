422dadad067a86ecf9014a09c2e4fcab982742ea
Esteban Arias <earias@ceibal.edu.uy>
2016-05-17 16:15:52 -0300

    comprime imagenes a 200

--- comprimir-imagenes.sh
+++ comprimir-imagenes.sh
@@ -1,1 +1,3 @@
-find . -name "*.png" -exec echo {} \; -exec convert {} -resize "100x100>" -write {} -thumbnail "100x100>" {} \;
+#PRE: sudo apt-get install imagemagick
+
+find . -name "*.png" -exec echo {} \; -exec convert {} -resize "200x200>" -write {} -thumbnail "200x200>" {} \;
